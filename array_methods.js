24df19527f1bb292f18cd1ae8a70cfeb0a9b1d50
frank-developer <franko122>
2025-09-02 12:25:55 +0000

    done

--- array_methods.js
+++ array_methods.js
@@ -79,4 +79,141 @@ console.log(wiThing);
 const iterator = users.entries();
 console.log(iterator.next().value);
 
-// 7
+//7 every \
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold)); 
+
+
+
+
+// 8 fill 
+// Fill with 0 from position 2 until position 4
+console.log(users.fill(0, 2, 4));
+// Expected output: Array [1, 2, 0, 0]
+
+// Fill with 5 from position 1
+console.log(users.fill(5, 1));
+// Expected output: Array [1, 5, 5, 5]
+
+console.log(users.fill(6));
+// Expected output: Array [6, 6, 6, 6]
+
+ 
+
+//  9 find  
+const array = [5, 12, 8, 130, 44];
+
+const found = array.find((element) => element > 10);
+
+console.log(found);
+// Expected output: 12  
+
+
+//  find IndxOf 
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  if (n % 2 === 0) {
+    return n === 2;
+  }
+  for (let factor = 3; factor * factor <= n; factor += 2) {
+    if (n % factor === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log([4, 6, 8, 9, 12].findIndex(isPrime));  
+console.log([4, 6, 7, 9, 12].findIndex(isPrime));  
+
+
+//  findLast  
+const   founds = users.findLast((element) => element > 45);
+
+console.log(found); 
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  if (n % 2 === 0) {
+    return n === 2;
+  }
+  for (let factor = 3; factor * factor <= n; factor += 2) {
+    if (n % factor === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log([4, 6, 8, 12].findLastIndex(isPrime));  
+console.log([4, 5, 7, 8, 9, 11, 12].findLastIndex(isPrime)); // 5
+
+
+//  flact 
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());  
+const arr2 = [0, 1, [2, [3, [4, 5]]]];
+
+console.log(arr2.flat()); 
+
+console.log(arr2.flat(2)); 
+
+console.log(arr2.flat(Infinity)); 
+
+
+
+// flatmap();
+const arr = [1, 2, 1];
+
+const result = arr.flatMap((num) => (num === 2 ? [2, 2] : 1));
+
+console.log(result); 
+
+
+// includes 
+
+console.log(users.includes(2)); 
+const pets = ["cat", "dog", "bat"];
+
+console.log(pets.includes("cat")); 
+
+console.log(pets.includes("at")); 
+
+
+
+// indexOf();
+const beasts = ["ant", "bison", "camel", "duck", "bison"]; 
+console.log(beasts.indexOf("bison")); 
+console.log(beasts.indexOf("bison", 2)); 
+console.log(beasts.indexOf("giraffe")); 
+
+// join()
+const elements = ["Fire", "Air", "Water"];
+
+console.log(elements.join()); 
+
+console.log(elements.join("")); 
+
+console.log(elements.join("-")); 
+
+// keys() 
+const iterators = users.keys();
+
+for (const key of iterators) {
+  console.log(key);
+} 
+
+//  last indexOf 
+const animals = ["Dodo", "Tiger", "Penguin", "Dodo"];
+
+console.log(animals.lastIndexOf("Dodo")); 
+
+console.log(animals.lastIndexOf("Tiger")); 
